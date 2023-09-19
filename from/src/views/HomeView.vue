56b46c6718286bca339b6fd06e6750b4bc9cb4fb
julio cesar <julioramirez.122veracruz@gmail.com>
2023-09-19 13:47:24 -0600

    first commit

---
+++ from/src/views/HomeView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <div  >
+      <nav>
+  <b-navbar  type="light" variant="info" >
+    <img src="@/assets/sigsa.png" id="ico" alt="User Icon" width="75" height="50">
+    <b-navbar-brand href="#" class=" text-center">SIGSA (Sistema De Informacion Geografica S.A de C.V )</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+  </b-navbar>
+</nav>
+</div>
+
+<!-- ddddd -->
+ <!-- Right aligned nav items -->
+ <b-navbar>
+  <div class="ml-auto d-flex">
+    <b-form-input class="mr-sm-2 form-control-sm" placeholder="ID" ></b-form-input>
+  </div>
+</b-navbar>
+<!-- xccc -->
+
+<!--  -->
+
+
+<!--  -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
